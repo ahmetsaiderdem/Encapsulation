46a1423ac39f652df7bd341705720264074317f3
erdem <erdemahmet750@gmail.com>
2021-07-25 13:50:50 +0300

    Proje dosyası ekle.

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            employee emp1 = new employee("vithal", 23);
+            employee emp2 = new employee(emp1);
+            Console.WriteLine(emp2.Details);
+            Console.ReadLine();
+        }
+    }
+
+    class employee
+    {
+        private string name;
+        private int age;
+
+        public employee(employee emp)
+        {
+            name = emp.name;
+            age = emp.age;
+
+        }
+        public employee(string name,int age)
+        {
+            this.name = name;
+            this.age = age;
+
+        }
+        public string Details
+        {
+            get
+            {
+                return "the age of " + name + " is " + age.ToString();
+            }
+        }
+    }
+
+}
